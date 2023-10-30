9778dfdc5670641cfc6cfc66a08db4d9c1f634ab
Rajesh Kumar <you@example.com>
2023-10-30 14:13:02 +0530

    episode1_inception

---
+++ App.js
@@ -0,0 +1,81 @@
+// const parent = React.createElement(
+//     "div",
+//     { id: "parent" },
+//     React.createElement(
+//         "div",
+//         { id: "child" },
+//         React.createElement(
+//             "h1",
+//             { },
+//             "I'm an h1 tag" 
+//         )
+        
+//     )
+// );
+
+// const parent = React.createElement(
+//     "div",
+//     { id: "parent" },
+
+//     React.createElement(
+//         "div",
+//         { id: "child" },
+//         [React.createElement(
+//             "h1",
+//             { },
+//             "I'm an h1 tag" 
+//         ),
+//         React.createElement(
+//             "h2",
+//             { },
+//             "I'm an h1 tag" 
+//         ),
+//         ]
+
+        
+//     )
+// );
+
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    
+    [React.createElement(
+        "div",
+        { id: "child" },
+        [React.createElement(
+            "h1",
+            { },
+            "I'm an h1 tag" 
+        ),
+        React.createElement(
+            "h2",
+            { },
+            "I'm an h2 tag" 
+        ),
+        ]),
+
+        React.createElement(
+            "div",
+            { id: "child2" },
+            [React.createElement(
+                "h1",
+                { },
+                "I'm an h1 tag" 
+            ),
+            React.createElement(
+                "h2",
+                { },
+                "I'm an h2 tag" 
+            ),
+            ]),
+        ]);
+
+
+//-------------------------
+const heading = React.createElement("h1", {id: "heading", class:"myh1", xyz:"abc"}, "Hello World from React!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+console.log(parent);
+
---
+++ README.md
@@ -0,0 +1,25 @@
+# Day1: React Introduction
+
+Using plain html how we will write "Hello World" in browser
+--> Emmet(It gives some template code)
+--> CDN(content delievery networks) These are the website where libraries are hosted and using cdn links we are just fetching that library in our project.
+
+Ex# Cdn link for jquery, react, sweetAlert2, leafletjs, googlemap, fontawesome
+Get React into our project, as browser don't understand react code directly.
+document, createElement, appendChild, getElementById .. ect these are already available in the browser.
+
+write "cdn react"
+
+ReactElement(Object) ==> HTML(browser understands)
+
+Can be single child either in array of childs if an html element have more than one childeren
+
+At the end of day ReactElement is an Object.
+
+createElement(name_of_the_tag, attributes, childerens)
+
+First childeren could be a normal react element
+if you have given multiple childerens, then you have to pass that ReactElement using an array.
+
+
+render() is replacing data when something getting render.
